7d8048e092a6d400f4cf2f35b966c9635c2558e1
Francisco Pineda <fpinedah2000@alumno.ipn.mx>
2025-07-18 14:11:19 -0600

    fix(web): corrige nombre en la pestaña de navegador & practicas OK

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Práctica 19 - Sistema de Citas</title>
+    <title>Práctica 22 - Sistema de Citas</title>
     
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
